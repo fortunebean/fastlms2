679f10fe9a7bb7cd063207c74327c2a1d240e9fa
fortunebean <89978842+fortunebean@users.noreply.github.com>
2021-09-18 17:44:30 +0900

    fastlmas초기설정

--- src/main/java/com/jerobase/fastlms/ServletInitializer.java
+++ src/main/java/com/jerobase/fastlms/ServletInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+    //2021 09-18
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
